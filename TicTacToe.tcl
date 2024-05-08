a0623b67dd7e4035f79849f880f9519af9b92438
Yannick De Figueiredo <pccurious@gmail.com>
2024-05-08 19:14:28 -0100

    Update TicTacToe.tcl
    Added DoDisplay Procedure

--- TicTacToe.tcl
+++ TicTacToe.tcl
@@ -1,4 +1,14 @@
 package require Tk
 package require chatwidget
 
+proc DoDisplay {} {
+	wm title . $::S(title)
+	frame .ctrl -relief ridge -bd 2 -padx 5 -pady 5
+	canvas .c -relief raised -bd 2 -height 500 -width 500 -highlightthickness 0
+	pack .c -side top -fill both -expand 1
+	pack .ctrl -side top -fill both
 
+	bind all <Key-F2> {console show}
+	bind .c <Configure> {ReCenter %W %h %w}
+	DoCtrlFrame
+}
